c82e7184bc65fd147b653aa7ee72c6faac8e8410
Xart3mis <ihavemanywaifus@protonmail.com>
2022-09-28 21:34:49 +0200

    add RegisterGlobalHotekey and UnregisterGlobalHotkeys to block some hotkeys [NOT WORKING]

--- go.mod
+++ go.mod
@@ -2,4 +2,16 @@ module GoHkar
 
 go 1.19
 
-require github.com/go-gl/glfw/v3.3/glfw v0.0.0-20220806181222-55e207c401ad
+require (
+	github.com/andrebq/gas v0.0.0-20140712172948-a3a07b0396b5
+	github.com/go-gl/gl v0.0.0-20211210172815-726fda9656d6
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20220806181222-55e207c401ad
+	github.com/go-gl/gltext v0.0.0-20170328174336-01a355945a70
+	github.com/go-gl/glu v0.0.0-20150315173544-b54aa06bc77a
+)
+
+require (
+	github.com/go-gl-legacy/glu v0.0.0-20150315173544-b54aa06bc77a // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	golang.org/x/image v0.0.0-20220902085622-e7cb96979f69 // indirect
+)
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"runtime"
 	"unsafe"
 
@@ -9,19 +10,37 @@ import (
 	"github.com/go-gl/glfw/v3.3/glfw"
 )
 
+var u32dll, _ = syscall.LoadLibrary("user32.dll")
+var ShowWindow, _ = syscall.GetProcAddress(u32dll, "ShowWindow")
+var SetWindowPos, _ = syscall.GetProcAddress(u32dll, "SetWindowPos")
+var RegisterHotkey, _ = syscall.GetProcAddress(u32dll, "RegisterHotKey")
+var UnregisterHotkey, _ = syscall.GetProcAddress(u32dll, "UnregisterHotkey")
+var SetWindowLongPtrW, _ = syscall.GetProcAddress(u32dll, "SetWindowLongPtrW")
+
+var MOD_ALT uint = 0x0001
+var MOD_CONTROL uint = 0x0002
+var MOD_NOREPEAT uint = 0x4000
+var MOD_SHIFT uint = 0x0004
+var MOD_WIN uint = 0x0008
+
+var VK_F4 uint = 0x73
+
+var HWND_TOPMOST int = -1
+var SWP_NOSIZE int = 0x0001
+var SWP_NOMOVE int = 0x0002
+var TOPMOST_FLAGS int = SWP_NOMOVE | SWP_NOSIZE
+
+var SW_HIDE int = 0
+var GWL_EXSTYLE int = -20
+var WS_EX_TOOLWINDOW int64 = 128
+
+var hotKeyId int = 0
+
 func init() {
 	runtime.LockOSThread()
 }
 
 func main() {
-	var u32dll, _ = syscall.LoadLibrary("user32.dll")
-	var ShowWindow, _ = syscall.GetProcAddress(u32dll, "ShowWindow")
-	var SetWindowLongPtrW, _ = syscall.GetProcAddress(u32dll, "SetWindowLongPtrW")
-
-	var SW_HIDE int = 0
-	var GWL_EXSTYLE int = -20
-	var WS_EX_TOOLWINDOW int64 = 128
-
 	err := glfw.Init()
 	if err != nil {
 		panic(err)
@@ -29,11 +48,13 @@ func main() {
 
 	defer glfw.Terminate()
 
-	// mode := glfw.GetPrimaryMonitor().GetVideoMode()
+	mode := glfw.GetPrimaryMonitor().GetVideoMode()
 
 	glfw.WindowHint(glfw.Floating, glfw.True)
 	glfw.WindowHint(glfw.Visible, glfw.False)
-	window, err := glfw.CreateWindow(1366, 768, "Testing", nil, nil)
+	glfw.WindowHint(glfw.AutoIconify, glfw.False)
+
+	window, err := glfw.CreateWindow(mode.Width, mode.Height, "Testing", nil, nil)
 	if err != nil {
 		panic(err)
 	}
@@ -50,15 +71,46 @@ func main() {
 	glfw.GetCurrentContext()
 	window.Show()
 
-	syscall.SyscallN(uintptr(ShowWindow), uintptr(unsafe.Pointer(hwnd)), uintptr(SW_HIDE))
-	syscall.SyscallN(uintptr(SetWindowLongPtrW), uintptr(unsafe.Pointer(hwnd)), uintptr(GWL_EXSTYLE), uintptr(WS_EX_TOOLWINDOW))
-	syscall.SyscallN(uintptr(ShowWindow), uintptr(unsafe.Pointer(hwnd)), uintptr(syscall.SW_SHOW))
+	syscall.SyscallN(SetWindowPos, uintptr(unsafe.Pointer(hwnd)), uintptr(HWND_TOPMOST), 0, 0, 0, 0, uintptr(TOPMOST_FLAGS))
+
+	syscall.SyscallN(ShowWindow, uintptr(unsafe.Pointer(hwnd)), uintptr(SW_HIDE))
+	syscall.SyscallN(SetWindowLongPtrW, uintptr(unsafe.Pointer(hwnd)), uintptr(GWL_EXSTYLE), uintptr(WS_EX_TOOLWINDOW))
+	syscall.SyscallN(ShowWindow, uintptr(unsafe.Pointer(hwnd)), uintptr(syscall.SW_SHOW))
+
 	window.SetOpacity(0.8)
+
+	RegisterGlobalHotkey(VK_F4, MOD_ALT|MOD_NOREPEAT, window)
+	defer UnregisterGlobalHotkeys()
+	// fmt.Scanln()
+
+	window.SetKeyCallback(KeyCallback)
 
 	for !window.ShouldClose() {
 
 		// Do OpenGL stuff.
 		window.SwapBuffers()
 		glfw.PollEvents()
+	}
+}
+
+func KeyCallback(w *glfw.Window, key glfw.Key, scancode int, action glfw.Action, mods glfw.ModifierKey) {
+	if action == glfw.Press || action == glfw.Repeat {
+		fmt.Println(glfw.GetKeyScancode(key))
+		if key == glfw.KeyQ && mods == glfw.ModControl+glfw.ModAlt+glfw.ModShift {
+			w.SetShouldClose(true)
+		}
+	}
+}
+
+func RegisterGlobalHotkey(key uint, mod uint, window *glfw.Window) {
+	hotKeyId++
+	_1, _2, err := syscall.SyscallN(RegisterHotkey, uintptr(unsafe.Pointer(nil)), uintptr(hotKeyId), uintptr(mod), uintptr(int16(key)))
+	fmt.Println(_1, _2, err)
+	fmt.Println(hotKeyId)
+}
+
+func UnregisterGlobalHotkeys() {
+	for i := 1; i < hotKeyId; i++ {
+		syscall.SyscallN(UnregisterHotkey, uintptr(i))
 	}
 }
