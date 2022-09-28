097e1c19c569319816984b621508d085f8551d1a
Xart3mis <ihavemanywaifus@protonmail.com>
2022-09-29 01:15:41 +0200

    new font; working on making window click-through

--- go.mod
+++ go.mod
@@ -5,10 +5,26 @@ go 1.19
 require (
 	github.com/go-gl/gl v0.0.0-20211210172815-726fda9656d6
 	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20220806181222-55e207c401ad
+	github.com/go-vgo/robotgo v0.100.10
 	github.com/nullboundary/glfont v0.0.0-20220309200611-a732e1465f7d
 )
 
 require (
+	github.com/StackExchange/wmi v1.2.1 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/lxn/win v0.0.0-20210218163916-a377121e959e // indirect
+	github.com/otiai10/gosseract v2.2.1+incompatible // indirect
+	github.com/robotn/gohook v0.31.3 // indirect
+	github.com/robotn/xgb v0.0.0-20190912153532-2cb92d044934 // indirect
+	github.com/robotn/xgbutil v0.0.0-20190912154524-c861d6f87770 // indirect
+	github.com/shirou/gopsutil v3.21.10+incompatible // indirect
+	github.com/tklauser/go-sysconf v0.3.9 // indirect
+	github.com/tklauser/numcpus v0.3.0 // indirect
+	github.com/vcaesar/gops v0.21.3 // indirect
+	github.com/vcaesar/imgo v0.30.0 // indirect
+	github.com/vcaesar/keycode v0.10.0 // indirect
+	github.com/vcaesar/tt v0.20.0 // indirect
 	golang.org/x/image v0.0.0-20220902085622-e7cb96979f69 // indirect
+	golang.org/x/sys v0.0.0-20211123173158-ef496fb156ab // indirect
 )
--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+//TODO: REFACTOR EVERYTHING
+
 import (
 	"fmt"
 	"log"
@@ -9,8 +11,11 @@ import (
 
 	"github.com/go-gl/gl/all-core/gl"
 	"github.com/go-gl/glfw/v3.3/glfw"
+	"github.com/go-vgo/robotgo"
 	"github.com/nullboundary/glfont"
 )
+
+type MousePos struct{ xpos, ypos float64 }
 
 var u32dll, _ = syscall.LoadLibrary("user32.dll")
 var ShowWindow, _ = syscall.GetProcAddress(u32dll, "ShowWindow")
@@ -34,9 +39,13 @@ var TOPMOST_FLAGS int = SWP_NOMOVE | SWP_NOSIZE
 
 var SW_HIDE int = 0
 var GWL_EXSTYLE int = -20
-var WS_EX_TOOLWINDOW int64 = 128
+var WS_EX_TOOLWINDOW uint = 0x00000080
+var WS_EX_LAYERED uint = 0x00080000
+var WS_EX_TRANSPARENT uint = 0x00000020
 
 var hotKeyId int = 0
+
+var CurrentMousePosition MousePos = MousePos{0, 0}
 
 func init() {
 	runtime.LockOSThread()
@@ -55,6 +64,11 @@ func main() {
 	glfw.WindowHint(glfw.ContextVersionMinor, 2)
 	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
 	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
+
+	glfw.WindowHint(glfw.RedBits, mode.RedBits)
+	glfw.WindowHint(glfw.GreenBits, mode.GreenBits)
+	glfw.WindowHint(glfw.BlueBits, mode.BlueBits)
+	glfw.WindowHint(glfw.RefreshRate, mode.RefreshRate)
 
 	glfw.WindowHint(glfw.TransparentFramebuffer, glfw.True)
 	glfw.WindowHint(glfw.AutoIconify, glfw.False)
@@ -82,7 +96,7 @@ func main() {
 	gl.DepthFunc(gl.LESS)
 	gl.ClearColor(0.0, 0.0, 0.0, 0.0)
 
-	font, err := glfont.LoadFont("Roboto-Light.ttf", int32(52), mode.Width, mode.Height)
+	font, err := glfont.LoadFont("./MedusaGothic.ttf", int32(52), mode.Width, mode.Height)
 	if err != nil {
 		log.Panicf("LoadFont: %v", err)
 	}
@@ -97,22 +111,27 @@ func main() {
 	syscall.SyscallN(SetWindowPos, uintptr(unsafe.Pointer(hwnd)), uintptr(HWND_TOPMOST), 0, 0, 0, 0, uintptr(TOPMOST_FLAGS))
 
 	syscall.SyscallN(ShowWindow, uintptr(unsafe.Pointer(hwnd)), uintptr(SW_HIDE))
-	syscall.SyscallN(SetWindowLongPtrW, uintptr(unsafe.Pointer(hwnd)), uintptr(GWL_EXSTYLE), uintptr(WS_EX_TOOLWINDOW))
+	syscall.SyscallN(SetWindowLongPtrW, uintptr(unsafe.Pointer(hwnd)), uintptr(GWL_EXSTYLE), uintptr(WS_EX_TOOLWINDOW|WS_EX_TRANSPARENT|WS_EX_LAYERED))
 	syscall.SyscallN(ShowWindow, uintptr(unsafe.Pointer(hwnd)), uintptr(syscall.SW_SHOW))
 
-	window.SetOpacity(1.0)
+	window.SetOpacity(0.8)
 
 	RegisterGlobalHotkey(VK_F4, MOD_ALT|MOD_NOREPEAT, window)
 	defer UnregisterGlobalHotkeys()
 
+	window.SetMouseButtonCallback(MouseButtonCallback)
+	window.SetCursorPosCallback(CursorPosCallback)
 	window.SetKeyCallback(KeyCallback)
+	window.SetScrollCallback(ScrollCallback)
+
+	text := "You've Been Pwned"
 
 	for !window.ShouldClose() {
 		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 		gl.ClearColor(0, 0, 0, 0)
 
-		font.SetColor(1.0, 1.0, 1.0, 1.0)                                                                 //r,g,b,a font color
-		font.Printf(float32(mode.Width)/2-font.Width(1.0, "Hkar")/2, float32(mode.Height)/2, 1.0, "Hkar") //x,y,scale,string,printf args
+		font.SetColor(1.0, 1.0, 1.0, 1.0)
+		font.Printf(float32(mode.Width)/2-font.Width(1.0, text)/2, float32(mode.Height)/2, 1.0, text)
 
 		window.SwapBuffers()
 		glfw.PollEvents()
@@ -126,6 +145,35 @@ func KeyCallback(w *glfw.Window, key glfw.Key, scancode int, action glfw.Action,
 			w.SetShouldClose(true)
 		}
 	}
+}
+
+func ScrollCallback(w *glfw.Window, xoff float64, yoff float64) {
+	robotgo.Scroll(int(xoff), int(yoff))
+	fmt.Println(xoff, yoff)
+}
+
+func MouseButtonCallback(w *glfw.Window, button glfw.MouseButton, action glfw.Action, mods glfw.ModifierKey) {
+	if action == 0 && button == 0 {
+		// robotgo.Toggle("left", "up")
+		fmt.Println("Left Click Release")
+	}
+	if action == 1 && button == 1 {
+		// robotgo.Toggle("right", "down")
+		fmt.Println("Right Click Press")
+	}
+	if action == 0 && button == 1 {
+		// robotgo.Toggle("right", "up")
+		fmt.Println("Right Click Release")
+	}
+	if action == 1 && button == 0 {
+		// robotgo.Toggle("left", "down")
+		fmt.Println("Left Click Press")
+	}
+}
+
+func CursorPosCallback(w *glfw.Window, xpos float64, ypos float64) {
+	CurrentMousePosition.xpos = xpos
+	CurrentMousePosition.ypos = ypos
 }
 
 func RegisterGlobalHotkey(key uint, mod uint, window *glfw.Window) {
