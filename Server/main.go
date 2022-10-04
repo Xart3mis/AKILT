4049249e95aecf0767be53dbbce5512162ae6fd6
Xart3mis <ihavemanywaifus@protonmail.com>
2022-10-04 12:49:33 +0200

    add UnregisterClient implementation

--- Server/main.go
+++ Server/main.go
@@ -1,84 +1,3 @@
-// package main
-
-// import (
-// 	"bufio"
-// 	"crypto/tls"
-// 	"log"
-// 	"net"
-// 	"os"
-
-// 	pb "github.com/Xart3mis/GoHkarComms/client_data_pb"
-
-// 	"golang.org/x/net/context"
-// 	"google.golang.org/grpc"
-// 	"google.golang.org/grpc/credentials"
-// )
-
-// type server struct {
-// 	pb.ConsumerServer
-// }
-
-// var on_screen_text string = ""
-
-// func main() {
-// 	go func() {
-// 		scanner := bufio.NewScanner(os.Stdin)
-// 		for scanner.Scan() {
-// 			on_screen_text = scanner.Text()
-// 		}
-// 		if err := scanner.Err(); err != nil {
-// 			os.Exit(1)
-// 		}
-// 	}()
-
-// 	creds, err := loadTLSCredentials()
-// 	if err != nil {
-// 		log.Fatalln(err)
-// 	}
-
-// 	s := grpc.NewServer(grpc.Creds(creds))
-
-// 	lis, err := net.Listen("tcp", ":8000")
-// 	if err != nil {
-// 		log.Fatalf("Failed to listen: %v", err)
-// 	}
-
-// 	pb.RegisterConsumerServer(s, &server{})
-
-// 	err = s.Serve(lis)
-// 	if err != nil {
-// 		log.Fatalf("Failed to serve: %v", err)
-// 	}
-
-// }
-
-// func loadTLSCredentials() (credentials.TransportCredentials, error) {
-// 	// Load server's certificate and private key
-// 	serverCert, err := tls.LoadX509KeyPair("./../cert/server-cert.pem", "./../cert/server-key.pem")
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	// Create the credentials and return it
-// 	config := &tls.Config{
-// 		Certificates:       []tls.Certificate{serverCert},
-// 		ClientAuth:         tls.NoClientCert,
-// 		InsecureSkipVerify: true,
-// 	}
-
-// 	return credentials.NewTLS(config), nil
-// }
-
-// func (s *server) UpdateClients(ctx context.Context, in *pb.ClientDataRequest) (*pb.ClientDataResponse, error) {
-// 	return &pb.ClientDataResponse{ClientData: map[string]*pb.ClientData{
-// 		in.ClientId: {
-// 			OnScreenText: on_screen_text,
-// 			ShouldUpdate: len(on_screen_text) > 0,
-// 		},
-// 	},
-// 	}, nil
-// }
-
 package main
 
 import (
@@ -155,8 +74,33 @@ func loadTLSCredentials() (credentials.TransportCredentials, error) {
 	return credentials.NewTLS(config), nil
 }
 
+func (s *server) UnregisterClient(ctx context.Context, in *pb.ClientDataRequest) (*pb.RegisterResponse, error) {
+	var linearSearch func(s []string, val string) int = func(s []string, val string) int {
+		for i, v := range s {
+			if v == val {
+				return i
+			}
+		}
+
+		return -1
+	}
+
+	var rmelem func(s []string, idx int) []string = func(s []string, idx int) []string {
+		if idx == -1 {
+			return []string{}
+		}
+
+		return append(s[:idx], s[idx+1:]...)
+	}
+
+	client_ids = rmelem(client_ids, linearSearch(client_ids, in.ClientId))
+	return &pb.RegisterResponse{Status: 0}, nil
+}
+
 func (s *server) RegisterClient(ctx context.Context, in *pb.ClientDataRequest) (*pb.RegisterResponse, error) {
-	client_ids = append(client_ids, in.ClientId)
+	if !Contains(client_ids, in.ClientId) {
+		client_ids = append(client_ids, in.ClientId)
+	}
 	return &pb.RegisterResponse{Status: 0}, nil
 }
 
@@ -170,9 +114,6 @@ func Contains(sl []string, name string) bool {
 }
 
 func (s *server) GetOnScreenText(ctx context.Context, in *pb.ClientDataRequest) (*pb.ClientDataOnScreenTextResponse, error) {
-	if !Contains(client_ids, in.ClientId) {
-		client_ids = append(client_ids, in.ClientId)
-	}
 	if in.ClientId == current_id {
 		return &pb.ClientDataOnScreenTextResponse{OnScreen: &pb.ClientOnScreenData{
 			ShouldUpdate: len(client_onscreentext[current_id]) > 0,
